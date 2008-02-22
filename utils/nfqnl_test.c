be78dbbc22e4c033ab9231ac01b4758e4b6d41ff
/C=EU/ST=EU/CN=Pablo Neira Ayuso/emailAddress=pablo@netfilter.org </C=EU/ST=EU/CN=Pablo Neira Ayuso/emailAddress=pablo@netfilter.org>
2008-02-22 23:21:37 +0000

    Signed-off-by: Paul P. Komkoff fix compilation issues on newer toolchains

--- utils/nfqnl_test.c
+++ utils/nfqnl_test.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <netinet/in.h>
+#include <linux/types.h>
 #include <linux/netfilter.h>		/* for NF_ACCEPT */
 
 #include <libnetfilter_queue/libnetfilter_queue.h>
